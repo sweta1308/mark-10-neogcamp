a707eb766f46e6a1f2ef35842e5f4676e65868d2
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2022-08-27 16:03:33 +0530

    docs: update project

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkBtn = document.querySelector("#check-btn");
-const errorMsg = document.querySelector("#error-message");
+const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll("#no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 1];
 
-checkBtn.addEventListener("click", function validateBillAndCashAmouunt() {
+checkBtn.addEventListener("click", function validateBillAndCashAmount() {
     hideMsg();
     if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
             const amountToBeReturned = cashGiven.value - billAmount.value;
             calculateChange(amountToBeReturned);
-        }   else {
-            showMsg("Cash given should be greater than or equal to the bill amount.");
+        } else {
+            showMsg("Cash given should be greater than or equal to the bill amount.")
         }
-    }   else {
+
+    } else {
         showMsg("Invalid Bill Amount");
     }
 });
+
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i];);
+        amountToBeReturned %= availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
 
 function hideMsg() {
     message.style.display = "none";
@@ -27,12 +36,4 @@ function hideMsg() {
 function showMsg(msg) {
     message.style.display = "block";
     message.innerText = msg;
-}
-
-function calculateChange(amountToBeReturned) {
-    for (let i = 0; i < availableNotes.length; i++); {
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
-        amountToBeReturned = amountToBeReturned % availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
-    }
 }
