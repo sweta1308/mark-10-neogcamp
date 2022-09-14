2b6c5d487dda20d71c58449d2edf0150db6b5375
Sweta Agarwalla <105535990+sweta1308@users.noreply.github.com>
2022-09-14 12:45:01 +0530

    update project

--- index.js
+++ index.js
@@ -4,17 +4,22 @@ const checkBtn = document.querySelector("#check-btn");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
+
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-checkBtn.addEventListener("click", function validateBillAndCashAmount() {
-    const billAmt = Number(billAmount.value);
-    const cashAmt = Number(cashGiven.value);
+
+checkBtn.addEventListener("click", ()=> {
+    var billAmt = Number(billAmount.value);
+    var cashAmt = Number(cashGiven.value);
     hideMsg();
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+    if (billAmt > 0) {
+        if (cashAmt > billAmt) {
+            const amountToBeReturned = cashAmt - billAmt;
             calculateChange(amountToBeReturned);
+        } else if (cashAmt == billAmt) {
+            showMsg("No change required");
         } else {
+            changeTable.style.display="none";
             showMsg("Cash given should be greater than or equal to the bill amount.")
         }
 
@@ -24,8 +29,8 @@ checkBtn.addEventListener("click", function validateBillAndCashAmount() {
 });
 
 function calculateChange(amountToBeReturned) {
-    for (let i = 0; i < availableNotes.length; i++) {
-        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+    for (var i = 0; i < availableNotes.length; i++) {
+        var numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
         amountToBeReturned %= availableNotes[i];
         noOfNotes[i].innerText = numberOfNotes;
     }
